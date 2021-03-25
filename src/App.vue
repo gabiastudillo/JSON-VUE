d39cd38c12f0136ceb66204800597cada231a585
gabiastudillo <gabrielalisseth@outlook.es>
2021-03-25 10:16:10 -0500

    Primera versión de la tabla con bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- Muestro el componente 'Datos' en el template de 'App.vue' -->
+    <Datos/>
   </div>
 </template>
-
+ 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+ 
+// Importo el componente 'Datos' 
+import Datos from './components/Datos.vue'
+ 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Datos
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Datos.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="table-responsive">
+          <table class="table">
+            <thead class="thead-dark">
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Precio</th>
+                <th scope="col">Stock</th>
+                <th scope="col">Imagen</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="item in items" v-bind:key="item.id">
+                <th scope="row">{{ item.id }}</th>
+                <td>{{ item.nombre }}</td>
+                <td>{{ item.precio }}</td>
+                <td>{{ item.stock }}</td>
+                <td>
+                  <img
+                    v-bind:src="'img/' + item.img"
+                    class="img-fluid"
+                    width="80px"
+                    v-bind:alt="item.nombre"
+                  />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// Archivo de datos 'postres.json'
+import datos from "../assets/json/postres.json";
+
+export default {
+  name: "Datos",
+  props: {
+    msg: String,
+  },
+
+  // Creo un método llamado 'items' y obtengo los datos con el método map()
+  computed: {
+    items() {
+      return datos.map((item) => {
+        return item;
+      });
+    },
+  },
+};
+</script>
